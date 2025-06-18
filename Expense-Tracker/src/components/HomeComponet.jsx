8e8c604f7f8ac1ebb097f250873983e918f62ebb
Safwan <safwanmv007@gmail.com>
2025-06-18 10:12:08 +0530

    feat: render OverviewComponent and TransactionComponent inside HomeComponent

--- Expense-Tracker/src/components/HomeComponet.jsx
+++ Expense-Tracker/src/components/HomeComponet.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components';
+import OverviewComponent from './OverviewComponent';
+import TransactionComponent from './TransactionComponent';
+const Container=styled.div`
+    font-family: Montserrat;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 30px 0 10px;
+    width: 360px;
+`
 
 function HomeComponet() {
+
+    const [transaction,updateTransaction]=useState([]);
+    const [expense,updateExpense]=useState(0);
+    const [income,updateIncome]=useState(0);
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <OverviewComponent />
+      <TransactionComponent/>
+    </Container>
   )
 }
 
